100a782ed3cfc81794ca97701df4b8e6677c527c
HcGonz441 <hector.gonzalez@iest.edu.mx>
2023-01-20 16:48:33 -0600

    add Modelo Noticias

---
+++ src/api/reporte/controllers/reporte.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * reporte controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::reporte.reporte');
---
+++ src/api/reporte/services/reporte.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * reporte service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::reporte.reporte');
