2657e4590f11fca258f519dae21b0b7d5e54b498
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-01-23 12:52:23 -0600

    23 Jan 2023 12:52

--- config/database.js
+++ config/database.js
@@ -1,13 +1,16 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', 'dpg-cf3efs1gp3jl0q45bhhg-a.oregon-postgres.render.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'observatorio_strapi'),
-      user: env('DATABASE_USERNAME', 'observatorio_strapi_user'),
-      password: env('DATABASE_PASSWORD', 'ys6hSf5H22eZkSoJqJt0DBgPNlZDcSAL'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env(
+        "DATABASE_HOST",
+        "dpg-cf3efs1gp3jl0q45bhhg-a.oregon-postgres.render.com"
+      ),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "observatorio_strapi"),
+      user: env("DATABASE_USERNAME", "observatorio_strapi_user"),
+      password: env("DATABASE_PASSWORD", "ys6hSf5H22eZkSoJqJt0DBgPNlZDcSAL"),
+      ssl: env.bool("DATABASE_SSL", true),
     },
   },
 });
